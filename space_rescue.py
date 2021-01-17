125ab55d448a01141499f3537937fbd4ef76c7a8
sshivani02 <72823107+sshivani02@users.noreply.github.com>
2021-01-16 23:22:18 -0800

    Add files via upload

--- space_rescue.py
+++ space_rescue.py
@@ -1,52 +1,53 @@
 import pygame, random, sys
 from pygame.locals import *
 
+# graphics/background
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
 TEXTCOLOR = (255, 255, 255)
 BACKGROUNDCOLOR = (0, 0, 50)
 FPS = 40
-BADDIEMINSIZE = 30
-BADDIEMAXSIZE = 40
-BADDIEMINSPEED = 1
-BADDIEMAXSPEED = 8
-ADDNEWBADDIERATE = 6
+MINSIZE = 30
+MAXSIZE = 40
+MINSPEED = 1
+MAXSPEED = 8
+ADDNEWRATE = 6
 PLAYERMOVERATE = 5
 
-def terminate():
+def terminate(): # for ending/exiting the game
     pygame.quit()
     sys.exit()
 
-def waitForPlayerToPressKey():
+def waitForPlayerToPressKey(): # press key to begin game
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
-                terminate()
+                terminate() # end
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE: # pressing escape quits
                     terminate()
                 return
 
-def playerHasHitBaddie(playerRect, baddies):
-    for b in baddies:
-        if playerRect.colliderect(b['rect']):
+def playerHasHitObstacle(playerRect, obstacles): # if it collides with an obstacle return true
+    for o in obstacles:
+        if playerRect.colliderect(o['rect']):
             return True
     return False
 
-def drawText(text, font, surface, x, y):
+def drawText(text, font, surface, x, y): # text/graphic stuff
     textobj = font.render(text, 1, TEXTCOLOR)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
-def obstacles():
+def obstacle_choose(): # randomly generate obstacle type: either asteroid or meteor
     num = random.randint(0,1)
     if num == 0:
         return pygame.image.load('asteroid.png')
     elif num == 1:
         return pygame.image.load('meteor.png')
 
-def aliens():
+def alien_choose(): # randomly generate aliens: 3 alien type
     num = random.randint(0,2)
     if num == 0:
         return pygame.image.load('alien1.png')
@@ -56,10 +57,9 @@ def aliens():
         return pygame.image.load('alien3.png')
 
 
-def ifPlayerHitsGood(playerRect, healthy):
-    # check if it hit
-    for h in healthy:
-        if playerRect.colliderect(h['rect']):
+def ifPlayerHitsAlien(playerRect, aliens): # if player encounters alien
+    for a in aliens:
+        if playerRect.colliderect(a['rect']):
             return True
     return False
 
@@ -76,7 +76,7 @@ font = pygame.font.SysFont('cambria', 23)
 title_font = pygame.font.SysFont('impact', 50)
 
 # set up sounds
-gameOverSound = pygame.mixer.Sound('gameover.wav')
+gameOverSound = pygame.mixer.Sound('gameover.wav') # sets the sound effect for game ending
 
 # set up images
 playerImage = pygame.image.load('rocket.png')
@@ -84,26 +84,28 @@ playerRect = playerImage.get_rect()
 
 # show the "Start" screen
 drawText('Space Rescue!', title_font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3 - 150))
-drawText('Your job is to rescue the stranded aliens...', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 100)
-drawText('But avoid the asteroids and fiery meteors!', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 150)
-drawText('Good luck! The galaxy is counting on you!', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 200)
-drawText('Press any key to begin:', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 250)
+drawText('Your job is to rescue the stranded aliens...', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3))
+drawText('But avoid the asteroids and fiery meteors!', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 50)
+drawText('Use the arrow keys to navigate.', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 100)
+drawText('Good luck! The galaxy is counting on you!', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 150)
+drawText('Press any key to begin:', font, windowSurface, (WINDOWWIDTH / 3 - 150), (WINDOWHEIGHT / 3) + 200)
+
 
 pygame.display.update()
 waitForPlayerToPressKey()
 
 
-topScore = 0
+topScore = 0 # initialize top score to 0
 while True:
     # set up the start of the game
-    baddies = []
-    healthy = []
+    obstacles = []
+    aliens = []
     score = 0
     playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
     moveLeft = moveRight = moveUp = moveDown = False
     reverseCheat = slowCheat = False
-    baddieAddCounter = 0
-    goodAddCounter = 0
+    obstacleAddCounter = 0
+    alienAddCounter = 0
 
     while True: # the game loop runs while the game part is playing
 
@@ -111,25 +113,25 @@ while True:
             if event.type == QUIT:
                 terminate()
 
-            if event.type == KEYDOWN:
+            if event.type == KEYDOWN: # Key commands
                 if event.key == ord('z'):
                     reverseCheat = True # make it go in reverse
                 if event.key == ord('x'):
                     slowCheat = True # make everything go slow motion
-                if event.key == K_LEFT:
+                if event.key == K_LEFT: # left arrow
                     moveRight = False
                     moveLeft = True
-                if event.key == K_RIGHT:
+                if event.key == K_RIGHT: # right arrow
                     moveLeft = False
                     moveRight = True
-                if event.key == K_UP:
+                if event.key == K_UP: # up arrow
                     moveDown = False
                     moveUp = True
-                if event.key == K_DOWN:
+                if event.key == K_DOWN: # down arrow
                     moveUp = False
                     moveDown = True
 
-            if event.type == KEYUP:
+            if event.type == KEYUP: # if key is not being pressed anymore
                 if event.key == ord('z'):
                     reverseCheat = False
                     score = 0
@@ -148,30 +150,30 @@ while True:
                 if event.key == K_DOWN:
                     moveDown = False
 
-        # Add new baddies at the top of the screen, if needed.
+        # Add new obstacles at the top of the screen, if needed.
         if not reverseCheat and not slowCheat:
-            baddieAddCounter += 1
-        if baddieAddCounter == ADDNEWBADDIERATE:
-            baddieAddCounter = 0
-            baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
-            newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-baddieSize), 0 - baddieSize, baddieSize, baddieSize),
-                        'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
-                        'surface':pygame.transform.scale(obstacles(), (baddieSize, baddieSize)),
+            obstacleAddCounter += 1
+        if obstacleAddCounter == ADDNEWRATE:
+            obstacleAddCounter = 0
+            obstacleSize = random.randint(MINSIZE, MAXSIZE)
+            newobstacle = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-obstacleSize), 0 - obstacleSize, obstacleSize, obstacleSize),
+                        'speed': random.randint(MINSPEED, MAXSPEED),
+                        'surface':pygame.transform.scale(obstacle_choose(), (obstacleSize, obstacleSize)),
                         }
 
-            baddies.append(newBaddie)
-        # good healthy food
+            obstacles.append(newobstacle)
+        # Add new aliens to the top of the screen
         if not reverseCheat and not slowCheat:
-            goodAddCounter += 1
-        if goodAddCounter == ADDNEWBADDIERATE:
-            goodAddCounter = 0
-            baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
-            newGood = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-baddieSize), 0 - baddieSize, baddieSize, baddieSize),
-                        'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
-                        'surface':pygame.transform.scale(aliens(), (baddieSize, baddieSize)),
+            alienAddCounter += 1
+        if alienAddCounter == ADDNEWRATE:
+            alienAddCounter = 0
+            alienSize = random.randint(MINSIZE, MAXSIZE)
+            newAlien = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-alienSize), 0 - alienSize, alienSize, alienSize),
+                        'speed': random.randint(MINSPEED, MAXSPEED),
+                        'surface':pygame.transform.scale(alien_choose(), (alienSize, alienSize)),
                         }
 
-            healthy.append(newGood)
+            aliens.append(newAlien)
         # Move the player around.
         if moveLeft and playerRect.left > 0:
             playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
@@ -185,35 +187,33 @@ while True:
         # Move the mouse cursor to match the player.
         pygame.mouse.set_pos(playerRect.centerx, playerRect.centery)
 
-        # Move the baddies down.
-        for b in baddies:
+        # Move the obstacles down.
+        for o in obstacles:
             if not reverseCheat and not slowCheat:
-                b['rect'].move_ip(0, b['speed'])
+                o['rect'].move_ip(0, o['speed'])
             elif reverseCheat:
-                b['rect'].move_ip(0, -5)
+                o['rect'].move_ip(0, -5)
             elif slowCheat:
-                b['rect'].move_ip(0, 1)
+                o['rect'].move_ip(0, 1)
 
-         # Delete baddies that have fallen past the bottom.
-        for b in baddies[:]:
-            if b['rect'].top > WINDOWHEIGHT:
-                baddies.remove(b)
+         # Delete obstacles that have fallen past the bottom.
+        for o in obstacles[:]:
+            if o['rect'].top > WINDOWHEIGHT:
+                obstacles.remove(o)
 
-        # GOODIES
-
-         # Move the goodies down.
-        for h in healthy:
+         # Move the aliens down.
+        for a in aliens:
             if not reverseCheat and not slowCheat:
-                h['rect'].move_ip(0, h['speed'])
+                a['rect'].move_ip(0, a['speed'])
             elif reverseCheat:
-                h['rect'].move_ip(0, -5)
+                a['rect'].move_ip(0, -5)
             elif slowCheat:
-                h['rect'].move_ip(0, 1)
+                a['rect'].move_ip(0, 1)
 
-         # Delete baddies that have fallen past the bottom.
-        for h in healthy[:]:
-            if h['rect'].top > WINDOWHEIGHT:
-                healthy.remove(h)
+         # Delete aliens that have fallen past the bottom.
+        for a in aliens[:]:
+            if a['rect'].top > WINDOWHEIGHT:
+                aliens.remove(a)
 
         # Draw the game world on the window.
         windowSurface.fill(BACKGROUNDCOLOR)
@@ -225,30 +225,32 @@ while True:
         # Draw the player's rectangle
         windowSurface.blit(playerImage, playerRect)
 
-        # Draw each baddie
-        for b in baddies:
-            windowSurface.blit(b['surface'], b['rect'])
+        # Draw each obstacle
+        for o in obstacles:
+            windowSurface.blit(o['surface'], o['rect'])
         
-        # draw goodies
-        for h in healthy:
-            windowSurface.blit(h['surface'], h['rect'])
+        # draw aliens
+        for a in aliens:
+            windowSurface.blit(a['surface'], a['rect'])
 
         pygame.display.update()
 
-        # Check if any of the baddies have hit the player.
-        if playerHasHitBaddie(playerRect, baddies):
+        # Check if any of the obstacles have hit the player.
+        if playerHasHitObstacle(playerRect, obstacles):
             if score > topScore:
                 topScore = score # set new top score
-            break
-        if ifPlayerHitsGood(playerRect, healthy):
+            break # immediately end game
+
+        # Check if player has encountered any aliens.
+        if ifPlayerHitsAlien(playerRect, aliens):
             score += 10
-            for h in healthy[:]:
-                healthy.remove(h)
+            for a in aliens[:]:
+                aliens.remove(a) # remove all aliens for a moment
 
         mainClock.tick(FPS)
 
     # Stop the game and show the "Game Over" screen.
-    gameOverSound.play()
+    gameOverSound.play() # play end of game music
 
     drawText('GAME OVER', font, windowSurface, (WINDOWWIDTH / 2 - 65), (WINDOWHEIGHT / 3 + 50))
     drawText('Press a key to play again.', font, windowSurface, (WINDOWWIDTH / 2 - 130), (WINDOWHEIGHT / 3) + 100)
